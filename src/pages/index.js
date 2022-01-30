e45a127ee64fb36b7bcba013f754e39b62e3a94c
samuel46 <samuelmunyiri34@gmail.com>
2022-01-30 14:26:40 +0300

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ import GlobalStyle, {
   ContentWrapper,
 } from "components/LandingPage/landingPageStyles";
 import SalesSection from "components/LandingPage/SalesSection";
-import NavbarTwo from "components/LandingPage/NavBar";
+import Navbar from "components/LandingPage/NavBar";
 
 export default function LandingPage() {
   return (
@@ -57,7 +57,7 @@ export default function LandingPage() {
         {/* start app creative landing */}
         <AppWrapper>
           <DrawerProvider>
-            <NavbarTwo />
+            <Navbar />
           </DrawerProvider>
           <ContentWrapper>
             <Banner />
